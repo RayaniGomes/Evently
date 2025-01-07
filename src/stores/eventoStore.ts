d5f53e675a30a8d07ed925eea653bf6421f8d564
Rayani Gomes <rayanigomesrn100@gmail.com>
2025-01-07 15:14:35 -0300

    feat: create store do evento

---
+++ src/stores/eventoStore.ts
@@ -0,0 +1,22 @@
+import { Evento } from "@/interfaces";
+import api from "@/service/api";
+import { create } from "zustand";
+
+interface PropEventoStore {
+    eventos: Evento[];
+    getEventos: () => void;
+}
+
+export const useEvento = create<PropEventoStore>((set) => ({
+    eventos: [],
+
+    getEventos: async () => {
+        await api.get("/eventos")
+            .then((response) => {
+                set({ 
+                    eventos: response.data.data
+                })
+                console.log(response) 
+            })
+    }
+}))
