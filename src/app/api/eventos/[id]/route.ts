edab9e6e704e6e6a4e8bd22a6bb90feca039dad9
Rayani Gomes <rayanigomesrn100@gmail.com>
2025-01-09 18:11:18 -0300

    feat: get do evento por id

---
+++ src/app/api/eventos/[id]/route.ts
@@ -0,0 +1,33 @@
+import { dataEventos } from "@/monks/eventos_data.json";
+import { NextApiRequest } from "next";
+export async function GET(req: NextApiRequest) {
+
+    const id = req.url?.split("/").pop(); 
+    const evento = dataEventos.filter((evento) => evento.id == parseInt(id as string))[0];
+
+    if(evento == undefined) {
+        return new Response(
+            JSON.stringify({
+                data: 'Evento nao encontrado',
+            }),
+            {
+                status: 404,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+    }
+    
+    return new Response(
+        JSON.stringify({
+            data: evento,
+        }),
+        {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    );
+}
--- src/stores/eventoStore.ts
+++ src/stores/eventoStore.ts
@@ -5,6 +5,7 @@ import { create } from "zustand";
 interface PropEventoStore {
     eventos: Evento[];
     getEventos: () => void;
+    getEventoDetalhe: (id: number) => void;
 }
 
 export const useEvento = create<PropEventoStore>((set) => ({
@@ -13,9 +14,19 @@ export const useEvento = create<PropEventoStore>((set) => ({
     getEventos: async () => {
         await api.get("/eventos")
             .then((response) => {
-                set({ 
+                set({
+                    eventos: response.data.data
+                })
+            })
+    },
+
+    getEventoDetalhe: async (id: number) => {
+        await api.get(`/evento/${id}/`)
+            .then((response) => {
+                set({
                     eventos: response.data.data
                 })
             })
     }
+
 }))
