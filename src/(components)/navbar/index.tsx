316b8f11c60ce155d09a460cf3678ab7adf42cc5
Rayani Gomes <rayanigomesrn100@gmail.com>
2025-01-13 17:26:00 -0300

    feat: componente sobreNos

--- src/(components)/navbar/index.tsx
+++ src/(components)/navbar/index.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Image from "next/image";
 import Link from "next/link";
 import { ButtonMenuHamburger, Form, Login, Menu, Nav, NavMobile } from "./styled";
---
+++ src/app/quem-somos/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/(components)/navbar";
+import Sobre from "@/(components)/sobreNos";
+
+export default function QuemSomos() {
+    return (
+        <main>
+            <Navbar />
+            <Sobre />
+        </main>
+    );
+}
