64882def15a2a22cf35ccc87889cf1ca9c836845
Rayani Gomes <rayanigomesrn100@gmail.com>
2025-01-15 14:03:43 -0300

    feat: componente titulo

---
+++ src/(components)/titulo/styled.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+interface border {
+    color: string
+}
+
+export const ContainerTitulo = styled.div<border>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    hr {
+        width: 50% !important;
+        border: 3px solid var(${(props) => props.color}) !important;
+        border-radius: 10px !important;
+        margin: 0 auto !important;
+        opacity: 1 !important;
+
+        @media (max-width: 768px) {
+            border: 2px solid var(--branco) !important;
+        }
+    }
+`
