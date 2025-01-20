889838c42a6acc89c6c2a88973c0d8f87d1c0d2c
Rayani Gomes <68917577+RayaniGomes@users.noreply.github.com>
2025-01-20 15:52:46 -0300

    feat: coloquei um botão para limpar filtro

--- src/(components)/filtro/index.tsx
+++ src/(components)/filtro/index.tsx
@@ -2,46 +2,90 @@ import { useEvento } from "@/stores/eventoStore";
 import InputCheckbox from "../inputCheckbox";
 import { Section } from "./styled";
 import SetFiltro from "@/help/filtro";
+import { formatarData } from "@/help/funcoes";
 
 export function Filtro() {
     const { estados, cidades, tipos } = SetFiltro();
     const { filtroData, filtroEstado, filtroCidade, filtroTipo } = useEvento();
 
+    console.log(filtroCidade);
+    
+    const limparFiltro = () => {
+        filtroData(" ");
+        filtroEstado("");
+        filtroCidade("");
+        filtroTipo("");
+    };
+
     return (
         <Section>
-            <h5>Filtro</h5>
+            <form action={limparFiltro}>
+                <div className="header">
+                    <h5>Filtro</h5>
+                    <button>Limpar</button>
+                </div>
 
-            <form action="">
                 <div className="item-form">
-                    <label htmlFor="">Data: <hr /></label>
-                    <input type="date" onChange={(event) => filtroData(event.target.value)} />
+                    <label htmlFor="">
+                        Data: <hr />
+                    </label>
+                    <input
+                        type="date"
+                        onChange={(event) => {
+                            const dataFormatada = formatarData(event.target.value);
+                            filtroData(dataFormatada);
+                        }}
+                    />
                 </div>
                 <div className="item-form">
-                    <label htmlFor="">Esatado: <hr /></label>
-                    <div className="inputs">
+                    <label htmlFor="">
+                        Estado: <hr />
+                    </label>
+                    <div className="inputs uf">
                         {estados.map((uf) => (
-                            <InputCheckbox key={uf} id={uf} htmlFor={uf} label={uf} onClick={() => filtroEstado(uf)} />
+                            <InputCheckbox
+                                key={uf}
+                                id={uf}
+                                htmlFor={uf}
+                                label={uf}
+                                onClick={() => filtroEstado(uf)}
+                            />
                         ))}
                     </div>
                 </div>
                 <div className="item-form">
-                    <label htmlFor="">Cidade: <hr /></label>
-                    <div className="inputs">
+                    <label htmlFor="">
+                        Cidade: <hr />
+                    </label>
+                    <div className="inputs cidade">
                         {cidades.map((cidade) => (
-                            <InputCheckbox key={cidade} id={cidade} htmlFor={cidade} label={cidade} onClick={() => filtroCidade(cidade)} />
+                            <InputCheckbox
+                            key={cidade}
+                            id={cidade}
+                            htmlFor={cidade}
+                            label={cidade}
+                            onClick={() => filtroCidade(cidade)}
+                            />
                         ))}
                     </div>
                 </div>
-                <div className="item-form">
-                    <label htmlFor="">Tipos: <hr /></label>
+                <div className="item-form tipo">
+                    <label htmlFor="">
+                        Tipos: <hr />
+                    </label>
                     <div className="inputs">
                         {tipos.map((tipo) => (
-                            <InputCheckbox key={tipo} id={tipo} htmlFor={tipo} label={tipo} onClick={() => filtroTipo(tipo)} />
+                            <InputCheckbox
+                                key={tipo}
+                                id={tipo}
+                                htmlFor={tipo}
+                                label={tipo}
+                                onClick={() => filtroTipo(tipo)}
+                            />
                         ))}
                     </div>
                 </div>
             </form>
-
-        </Section >
+        </Section>
     );
 }
