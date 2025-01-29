2c08f7ff62c6322721f1c0da0cbd66771ebfa765
Rayani Gomes <rayanigomesrn100@gmail.com>
2025-01-29 18:19:45 -0300

    fix: respondividade da pagina login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,50 @@
-'use client';
+"use client";
 import Footer from "@/(components)/footer";
 import Navbar from "@/(components)/navbar";
 import { Section } from "./styled";
 import { Container } from "react-bootstrap";
 import FormLogin from "@/(components)/formularios/formLogin";
 import FormUsuario from "@/(components)/formularios/formUsuario";
+import { useState } from "react";
 
 export default function Login() {
-    return (
-        <main>
-            <Navbar />
-            <Container as={Section}>
-                <div className="form-login">
-                    <h3>Login</h3>
-                    <FormLogin />
-                </div>
-                <div className="hr" />
-                <div className="form-cadastre">
-                    <h3>Cadastre-se</h3>
-                    <FormUsuario /> 
-                </div>
-            </Container>  
-            <Footer />
-        </main>
-    );
+  const [ativarcadastro, setAtivarcadastro] = useState(false);
+
+  const toggleAtivarcadastro = () => {
+    setAtivarcadastro(!ativarcadastro);
+  };
+
+  return (
+    <main>
+      <Navbar />
+      <Container as={Section}>
+        <div className={!ativarcadastro ? "form-login" : "ativar form-login"}>
+          <h3>Login</h3>
+          <FormLogin />
+          <button
+            type="button"
+            className="btn-cadastre"
+            onClick={toggleAtivarcadastro}
+          >
+            <h6>Cadastre-se</h6>
+            <i className="bi bi-arrow-right" />
+          </button>
+        </div>
+        <div className="hr display-none" />
+        <div className={ativarcadastro ? "form-cadastre" : "ativar form-cadastre"}>
+          <h3>Cadastre-se</h3>
+          <FormUsuario />
+          <button
+            type="button"
+            className="btn-login"
+            onClick={toggleAtivarcadastro}
+          >
+            <i className="bi bi-arrow-left" />
+            <h6>Login</h6>
+          </button>
+        </div>
+      </Container>
+      <Footer />
+    </main>
+  );
 }
--- src/app/login/styled.ts
+++ src/app/login/styled.ts
@@ -2,13 +2,14 @@ import styled from "styled-components";
 
 export const Section = styled.section`
   width: 100%;
-  min-height: 700px;
+  height: 100%;
   display: flex;
   margin: 5rem auto;
   margin-top: 10rem;
   background: var(--gradiente-radial);
   border-radius: 10px;
   gap: 3rem;
+  color: var(--branco);
 
   .hr {
     max-width: 1px;
@@ -31,6 +32,61 @@ export const Section = styled.section`
     h3,
     i {
       color: var(--branco);
+    }
+  }
+
+  .btn-cadastre,
+  .btn-login {
+    display: none;
+    align-items: center;
+    justify-content: center;
+    background-color: transparent;
+    border: none;
+    color: var(--branco);
+    gap: 0.5rem;
+
+    h6 {
+      margin: 0;
+    }
+
+    i {
+      height: auto;
+      font-size: 18px;
+    }
+
+    &:hover {
+      opacity: 0.87;
+    }
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 100%;
+    
+    .form-login{
+      display: flex;
+      width: 100%;
+    }
+
+    .form-cadastre {
+      display: none;
+    }
+
+    .hr {
+      display: none;
+    }
+
+    .ativar .form-login {
+      display: none; 
+    }
+    
+    .ativar .form-cadastre {
+      display: flex;
+    }
+
+    .btn-cadastre,
+    .btn-login {
+      display: flex;
     }
   }
 `;
