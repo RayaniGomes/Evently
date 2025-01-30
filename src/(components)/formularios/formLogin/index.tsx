ceb8b476fbc2e2003c30beda50b07953d5430eb4
Rayani Gomes <rayanigomesrn100@gmail.com>
2025-01-30 15:27:32 -0300

    fix: validação do formLogin

--- src/(components)/formularios/formLogin/index.tsx
+++ src/(components)/formularios/formLogin/index.tsx
@@ -1,23 +1,33 @@
 import { useState } from "react";
 import { Form, GrupoInput } from "../formUsuario/styled";
 import Link from "next/link";
+import { createDataUsuario, usuarioSchema } from "@/schema/usuario.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 export default function FormLogin() {
   const [mostrarSenha, setMostrarSenha] = useState(false);
+  const { register, handleSubmit, formState: { errors } } = useForm<createDataUsuario>({ resolver: zodResolver(usuarioSchema) });
 
   const toggleMostrarSenha = () => {
     setMostrarSenha(!mostrarSenha);
   };
 
+  const onSubmit = (data: createDataUsuario) => {
+    console.log(data);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <GrupoInput>
         <i className="bi bi-person-fill" />
-        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Email" {...register("email")} />
       </GrupoInput>
+      {errors.email && <span>{errors.email.message}</span>}
+
       <GrupoInput>
         <i className="bi bi-lock-fill" />
-        <input type={mostrarSenha ? "text" : "password"} placeholder="Senha" />
+        <input type={mostrarSenha ? "text" : "password"} placeholder="Senha" {...register("senha")} />
         <button type="button" onClick={toggleMostrarSenha}>
           {mostrarSenha ? (
             <i className="bi bi-eye-slash-fill" />
