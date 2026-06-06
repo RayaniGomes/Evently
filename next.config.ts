9d47f240a2d1ef57c75e70420abef8fb3cb1c477
Rayani Gomes <68917577+RayaniGomes@users.noreply.github.com>
2026-06-06 12:12:59 -0300

    fix: update image configuration to use remotePatterns for better flexibility

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["encurtador.com.br", "github.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
   },
 };
 
